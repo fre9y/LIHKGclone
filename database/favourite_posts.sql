c158fc1b3388868a9638cd90b04761ec835268d1
freyy9 <0fr3ddy0@gmail.com>
2023-01-17 17:17:54 +0800

    frey9

--- database/favourite_posts.sql
+++ database/favourite_posts.sql
@@ -1,6 +1,10 @@
 create table favourite_posts(
     id serial primary key,
-    user_id integer not null,
-    post_id integer not null
+    user_id INTEGER not null,
+    FOREIGN KEY user_id REFERENCES users(id),
+    posts_id INTEGER not null,
+    FOREIGN KEY post_id REFERENCES posts(id),
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
 
 );
