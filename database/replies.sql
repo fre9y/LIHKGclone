ce65e8699710a2382b3b4d56125232890902b138
Anthony <antonyeung919@gmail.com>
2023-01-17 17:50:50 +0800

    edit

--- database/replies.sql
+++ database/replies.sql
@@ -5,14 +5,14 @@ CREATE TABLE replies (
     id SERIAL primary key,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
-    page_number INTEGER not null,
-    likes INTEGER not null,
-    dislikes INTEGER not null,
+    page_number INTEGER not null default 1,
+    likes INTEGER not null default 0,
+    dislikes INTEGER not null default 0,
     user_id INTEGER not null,
     FOREIGN KEY (user_id) REFERENCES users(id),
     post_id INTEGER not null,
     FOREIGN KEY (post_id) REFERENCES posts(id),
-    image_id INTEGER not null,
+    image_id INTEGER,
     FOREIGN KEY (image_id) REFERENCES images(id),
     content TEXT
 );
