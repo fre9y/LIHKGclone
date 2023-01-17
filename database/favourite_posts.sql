ae5c8aaba6aea1fae09312ceb876b8e09839ccf2
freyy9 <0fr3ddy0@gmail.com>
2023-01-17 17:38:52 +0800

    frey10

--- database/favourite_posts.sql
+++ database/favourite_posts.sql
@@ -1,10 +1,10 @@
 create table favourite_posts(
-    id serial primary key,
+    id SERIAL primary key,
     user_id INTEGER not null,
-    FOREIGN KEY user_id REFERENCES users(id),
-    posts_id INTEGER not null,
-    FOREIGN KEY post_id REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    post_id INTEGER not null,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
     created_at timestamp not null default now(),
-    updated_at timestamp not null default now(),
+    updated_at timestamp not null default now()
 
 );
