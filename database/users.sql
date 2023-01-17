dbbfd88904d157d5c85d9fd157a4c742350837e6
freyy9 <0fr3ddy0@gmail.com>
2023-01-17 15:57:15 +0800

    frey 5th

---
+++ database/users.sql
@@ -0,0 +1,30 @@
+create table users(
+    id serial primary key,
+    nickname varchar(255) not null,
+    email varchar(255) not null,
+    is_admin boolean default false not null,
+    is_p boolean default false not null,
+    is_male boolean default true not null,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null
+
+);
+
+
+
+insert into users
+(nickname, email, is_admin, is_p,is_male)
+values
+('csy', 'cyrus0222@gbd.com', false, false, true),
+('鴨鴨點啫', 'duckduck@abc.com', false, false, false),
+('連尼住', 'lineage@lihkg.com', true, false, true),
+('F9', 'fourier@cuhk.edu,hk', false, false, true),
+('meeee', 'admin@tecky.io', true, true, false),
+('yurayura', 'yurayura@123.456', false, true, false),
+('1機械人一', 'bot1@botbot.com', false, true, true),
+('2機械人二', 'bot2@botbot.com', false, true, true),
+('3機械人三', 'bot3@botbot.com', false, true, true)
+
+
+
+select * from users;
--- main.ts
+++ main.ts
@@ -1,31 +1,36 @@
 
 import express from 'express';
 import type { Request, Response } from "express";
-//import expressSession from "express-session";
+import { Client } from "pg";
+import dotenv from "dotenv";
+
 let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+dotenv.config();
 
+export const client = new Client({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
 
-app.use((req: Request, res, next) => {
-    try{
-        next(); 
-    } catch(error) {
-        res.status(500).json({
-            message: "[SECTION001]Server Error",
-        });
-        console.log(error);
-    }
-    
-});
+async function connectDatabase() {
+
+    await client.connect();
+    console.log("db connected");
+    await client.end();
+    console.log("db disconnected");
+}
+connectDatabase();
 
 
 app.use(express.static("public"));
 app.use(express.static("uploads")); //photos in folder can be found
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  res.send("Hello World"); 
 });
 
 app.use((req, res) => {
