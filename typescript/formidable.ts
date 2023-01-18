1f9e3421730d7183af563cb79ebace433b3dbd8f
Anthony <antonyeung919@gmail.com>
2023-01-18 16:02:43 +0800

    postsroute

---
+++ typescript/formidable.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import formidable from 'formidable'
+export const uploadDir = 'uploads'
+
+export const form = formidable({
+	uploadDir,
+	keepExtensions: true,
+	maxFiles: 1,
+	maxFileSize: 10 * 1024 ** 3,
+	filter: (part) => part.mimetype?.startsWith('image/') || false,
+	filename: (originalName, originalExt, part, form) => {
+		let fieldName = part.name
+		let timestamp = Date.now()
+		let ext = part.mimetype?.split('/').pop()
+		return `${fieldName}-${timestamp}.${ext}`
+	}
+})
+
+export function formParsePromise(req: express.Request) {
+	return new Promise<any>((resolve, reject) => {
+		form.parse(req, (err, fields, files) => {
+
+			if (err) {
+				reject(err)
+				return
+			}
+
+			resolve({
+				fields,
+				files
+			})
+		})
+	})
+}
