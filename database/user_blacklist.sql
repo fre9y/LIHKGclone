8d5c210e325a9f2db51aeb1a45afdc09a228da8d
freyy9 <0fr3ddy0@gmail.com>
2023-01-19 12:28:01 +0800

    frey19

---
+++ database/user_blacklist.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user_blacklist (
+    id SERIAL primary key,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    user_block_others INTEGER not null,
+    FOREIGN KEY (user_block_others) REFERENCES users(id),
+    user_being_blocked INTEGER not null,
+    FOREIGN KEY (user_being_blocked) REFERENCES users(id)
+);
+
+
+
+
+insert into user_blacklist
+(user_block_others, user_being_blocked)
+values
+(1,2),
+(2,1),
+(10,3),
+(3,4),
+(4,10);
--- database/users.sql
+++ database/users.sql
@@ -1,16 +1,23 @@
-/*
+
 create table users(
     id serial primary key,
     nickname varchar(255) not null unique,
     email varchar(255) not null unique,
     is_admin boolean default false not null,
-    is_p boolean default false not null,
+    is_p boolean default true not null,
     is_male boolean default true not null,
     created_at timestamp default now() not null,
     updated_at timestamp default now() not null
 
 );
-
+/*
+3 google accounts
+is_admin, is_p, !is_p
+- is_admin:true, is_p: (not important), created_at:(not important)
+admin override is_p
+- is_admin: false, is_p: false, created_at: old date (manual edit)
+- is_admin: false, is_p: true, created_at: new date (auto)
+*/
 
 
 insert into users
@@ -20,13 +27,16 @@ values
 ('鴨鴨點啫', 'duckduck@abc.com', false, false, false),
 ('連尼住', 'lineage@lihkg.com', true, false, true),
 ('F9', 'fourier@cuhk.edu.hk', false, false, true),
-('meeee', 'admin@tecky.io', true, true, false),
+('meeee', 'admin@tecky.io', true, false, false),
 ('yurayura', 'yurayura@123.com', false, true, false),
 ('1機械人一', 'bot1@botbot.com', false, true, true),
 ('2機械人二', 'bot2@botbot.com', false, true, true),
 ('3機械人三', 'bot3@botbot.com', false, true, true),
-('漢語多功能字庫', 'bbb@cuhk.edu.hk', false, true, false)
-*/
+('漢語多功能字庫', 'bbb@cuhk.edu.hk', false, true, false),
+('me', '0fr3ddy0@gmail.com', true, false, false);
 
 
--- select * from users;
+
+
+
+select * from users;
